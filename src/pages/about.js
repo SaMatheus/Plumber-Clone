210f8eaee66db1405571f7ec68a196dd69d8c22e
Matheus Sá <matheus.sa.alves@outlook.com>
2021-04-02 10:29:39 -0300

    Atualizando pointsData

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,3 +1,9 @@
+// STYLES
+import styles from '../styles/pages/About.module.css'
+
+// DATA
+import pointsData from '../data/pointsData'
+
 // COMPONENT
 import Header from '../components/Header'
 import Team from '../components/Team'
@@ -7,6 +13,44 @@ const About = () => {
   return (
     <div>
      <Header />
+     <section className={styles.aboutUsContainer}>
+       <span/>
+       <div className={styles.aboutUsContent}>
+         <h1>About Us</h1>
+         <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+          when an unknown printer took a galley of type and scrambled it to make a type 
+          specimen book
+         </p>
+       </div>
+     </section>
+     <section className={styles.pointsCompanyContainer}>
+       <div className={styles.pointsCompanyText}>
+         <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+          when an unknown printer took a galley of type and scrambled it to make a type 
+          specimen book. 
+         </p>
+         <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+          when an unknown printer took a galley of type and scrambled it to make a type 
+          specimen book. It has survived not only five centuries, but also the leap into 
+          electronic 
+         </p>
+         <p>
+          typesetting, remaining essentially unchanged. It was popularised in the 1960s 
+          with the release of Letraset sheets containing Lorem Ipsum passages, and more 
+          recently with desktop publishing software like Aldus PageMaker including versions 
+          of Lorem Ipsum.
+         </p>
+       </div>
+       <div className={styles.pointsCompanyGrid}>
+
+       </div>
+     </section>
      <Team />
      <Wrapper />
     </div>
