9858c69825a6764144cb7f61d151e5b0050dc41f
Matheus Sá <matheus.sa.alves@outlook.com>
2021-04-02 11:10:08 -0300

    Componente CommercialService finalizado

--- src/components/CommercialService.jsx
+++ src/components/CommercialService.jsx
@@ -2,9 +2,20 @@ import styles from '../styles/components/CommercialService.module.css'
 
 const CommercialService = () => {
   return (
-    <div>
-      
-    </div>
+    <section className={styles.commercialServiceContainer}>
+      <div className={styles.img}></div>
+      <div className={styles.content}>
+        <div>
+          <h1>Commercial Service</h1>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to make a type 
+            specimen book
+          </p>
+        </div>
+      </div>
+    </section>
   )
 }
 
