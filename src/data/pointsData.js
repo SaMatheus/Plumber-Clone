18e0ea4de8db6dd1fe3a818194d72336f769ad30
Matheus Sá <matheus.sa.alves@outlook.com>
2021-04-02 10:29:57 -0300

    Atualizando pointsData

--- src/data/pointsData.js
+++ src/data/pointsData.js
@@ -1,35 +1,43 @@
 const pointsData = [
   {
     id: 1,
-    title: 'Residental Service'
+    title: 'Residental Service',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text',
   },
   {
     id: 2,
-    title: 'Maintenance'
+    title: 'Maintenance',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text',
   },
   {
     id: 3,
-    title: 'Commercial Service'
+    title: 'Commercial Service',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text',
   },
   {
     id: 4,
-    title: 'Keep Your Home Safe'
+    title: 'Keep Your Home Safe',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text',
   },
   {
     id: 5,
-    title: 'Plumbing Expertise'
+    title: 'Plumbing Expertise',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text',
   },
   {
     id: 6,
-    title: 'Personalized Records'
+    title: 'Personalized Records',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text',
   },
   {
     id: 7,
-    title: 'Multiple Sites Covered'
+    title: 'Multiple Sites Covered',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text',
   },
   {
     id: 8,
-    title: 'Priority Scheduling'
+    title: 'Priority Scheduling',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text',
   },
 ]
 
