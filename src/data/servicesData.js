04a3ec246f94c7e8b4195b676b75832b5fc987f0
Matheus Sá <matheus.sa.alves@outlook.com>
2021-04-02 12:15:00 -0300

    Criando serviceData

---
+++ src/data/servicesData.js
@@ -0,0 +1,76 @@
+const servicesData = [
+  {
+    id: 1,
+    img: '/icons/services/icon1.svg',
+    title: 'Plumbing Service',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the.',
+  },
+  {
+    id: 2,
+    img: '/icons/services/icon2.svg',
+    title: 'Plumbing Service',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the.',
+  },
+  {
+    id: 3,
+    img: '/icons/services/icon3.svg',
+    title: 'Plumbing Service',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the.',
+  },
+  {
+    id: 4,
+    img: '/icons/services/icon4.svg',
+    title: 'Plumbing Service',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the.',
+  },
+  {
+    id: 5,
+    img: '/icons/services/icon2.svg',
+    title: 'Plumbing Service',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the.',
+  },
+  {
+    id: 6,
+    img: '/icons/services/icon3.svg',
+    title: 'Plumbing Service',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the.',
+  },
+  {
+    id: 7,
+    img: '/icons/services/icon4.svg',
+    title: 'Plumbing Service',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the.',
+  },
+  {
+    id: 8,
+    img: '/icons/services/icon1.svg',
+    title: 'Plumbing Service',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the.',
+  },
+  {
+    id: 9,
+    img: '/icons/services/icon3.svg',
+    title: 'Plumbing Service',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the.',
+  },
+  {
+    id: 10,
+    img: '/icons/services/icon2.svg',
+    title: 'Plumbing Service',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the.',
+  },
+  {
+    id: 11,
+    img: '/icons/services/icon4.svg',
+    title: 'Plumbing Service',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the.',
+  },
+  {
+    id: 12,
+    img: '/icons/services/icon1.svg',
+    title: 'Plumbing Service',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the.',
+  },
+]
+
+export default servicesData
