95bcc37d81d1f1c94fed13747566b0921e7fa747
Matheus Sá <matheus.sa.alves@outlook.com>
2021-04-05 11:00:37 -0300

    Pagina project criada

--- src/pages/project.js
+++ src/pages/project.js
@@ -1,6 +1,13 @@
+// STYLES
+import styles from '../styles/pages/Project.module.css'
+
 // COMPONENT
 import Header from '../components/Header'
+import CommercialService from '../components/CommercialService'
 import Wrapper from '../components/Wrapper'
+
+// DATA
+import projectData from '../data/projectData'
 
 // HOOKS
 import { useContext, useEffect } from 'react';
@@ -18,6 +25,30 @@ const Project = () => {
   return (
     <div>
       <Header />
+      <div className={styles.imageContainer} />
+      <section className={styles.projectContainer}>
+        <div className={styles.text}>
+          <h1>Our Projects</h1>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to make a type 
+            specimen book.
+          </p>
+        </div>
+        <div className={styles.grid}>
+          {projectData.map(project => {
+            return (
+              <div className={styles.project}>
+                <img src={project.img} alt=""/>
+                <h1>{project.title}</h1>
+                <p>{project.description}</p>
+              </div>
+            )
+          })}
+        </div>
+      </section>
+      <CommercialService />
       <Wrapper />
     </div>
   )
