8a4f98a4ea846cadda1792cfd30733d32f84b1cd
Matheus Sá <matheus.sa.alves@outlook.com>
2021-04-05 10:48:36 -0300

    Criando projectData

---
+++ src/data/projectData.js
@@ -0,0 +1,58 @@
+const projectData = [
+  {
+    id: 1,
+    img: '/icons/project/img1.png',
+    title: 'Residential',
+    description: 'Kitchen Plumbing Project'
+  },
+  {
+    id: 2,
+    img: '/icons/project/img2.png',
+    title: 'Townhouse',
+    description: 'External Plumbing Project'
+  },
+  {
+    id: 3,
+    img: '/icons/project/img3.png',
+    title: 'Industrial',
+    description: 'Machine Plumbing Project'
+  },
+  {
+    id: 4,
+    img: '/icons/project/img2.png',
+    title: 'Townhouse',
+    description: 'External Plumbing Project'
+  },
+  {
+    id: 5,
+    img: '/icons/project/img3.png',
+    title: 'Industrial',
+    description: 'Machine Plumbing Project'
+  },
+  {
+    id: 6,
+    img: '/icons/project/img1.png',
+    title: 'Residential',
+    description: 'Kitchen Plumbing Project'
+  },
+  {
+    id: 7,
+    img: '/icons/project/img3.png',
+    title: 'Industrial',
+    description: 'Machine Plumbing Project'
+  },
+  {
+    id: 8,
+    img: '/icons/project/img1.png',
+    title: 'Residential',
+    description: 'Kitchen Plumbing Project'
+  },
+  {
+    id: 9,
+    img: '/icons/project/img2.png',
+    title: 'Townhouse',
+    description: 'External Plumbing Project'
+  },
+]
+
+export default projectData
