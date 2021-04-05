dfc444987d0bf8afc52bcd0a4a28618a9b6b9d9a
Matheus Sá <matheus.sa.alves@outlook.com>
2021-04-05 12:36:08 -0300

    Criando blogData

---
+++ src/data/blogData.js
@@ -0,0 +1,40 @@
+const blogData = [
+  {
+    id: 1,
+    img: '/icons/blogs/img1.png',
+    title: 'Lorem Ipsum is simply and typesetting.',
+    description: 'Don’t wrestle with rig assembly and hot, noisy miners at home. We have the fastest bitcoin mining hardware running for you....'
+  },
+  {
+    id: 2,
+    img: '/icons/blogs/img2.png',
+    title: 'All you want to know about industrial laws.',
+    description: 'Don’t wrestle with rig assembly and hot, noisy miners at home. We have the fastest bitcoin mining hardware running for you....'
+  },
+  {
+    id: 3,
+    img: '/icons/blogs/img3.png',
+    title: 'Lorem Ipsum dummy text of the printing.',
+    description: 'Don’t wrestle with rig assembly and hot, noisy miners at home. We have the fastest bitcoin mining hardware running for you....'
+  },
+  {
+    id: 4,
+    img: '/icons/blogs/img2.png',
+    title: 'All you want to know about industrial laws.',
+    description: 'Don’t wrestle with rig assembly and hot, noisy miners at home. We have the fastest bitcoin mining hardware running for you....'
+  },
+  {
+    id: 5,
+    img: '/icons/blogs/img3.png',
+    title: 'Lorem Ipsum dummy text of the printing.',
+    description: 'Don’t wrestle with rig assembly and hot, noisy miners at home. We have the fastest bitcoin mining hardware running for you....'
+  },
+  {
+    id: 6,
+    img: '/icons/blogs/img1.png',
+    title: 'Lorem Ipsum is simply and typesetting.',
+    description: 'Don’t wrestle with rig assembly and hot, noisy miners at home. We have the fastest bitcoin mining hardware running for you....'
+  },
+]
+
+export default blogData
