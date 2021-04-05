2a3dad13b59d3edf8b4656f223dab78a8aa74597
Matheus Sá <matheus.sa.alves@outlook.com>
2021-04-05 12:41:48 -0300

    Acrescentando href

--- src/data/blogData.js
+++ src/data/blogData.js
@@ -3,37 +3,43 @@ const blogData = [
     id: 1,
     img: '/icons/blogs/img1.png',
     title: 'Lorem Ipsum is simply and typesetting.',
-    description: 'Don’t wrestle with rig assembly and hot, noisy miners at home. We have the fastest bitcoin mining hardware running for you....'
+    description: 'Don’t wrestle with rig assembly and hot, noisy miners at home. We have the fastest bitcoin mining hardware running for you....',
+    href: '#'
   },
   {
     id: 2,
     img: '/icons/blogs/img2.png',
     title: 'All you want to know about industrial laws.',
-    description: 'Don’t wrestle with rig assembly and hot, noisy miners at home. We have the fastest bitcoin mining hardware running for you....'
+    description: 'Don’t wrestle with rig assembly and hot, noisy miners at home. We have the fastest bitcoin mining hardware running for you....',
+    href: '#'
   },
   {
     id: 3,
     img: '/icons/blogs/img3.png',
     title: 'Lorem Ipsum dummy text of the printing.',
-    description: 'Don’t wrestle with rig assembly and hot, noisy miners at home. We have the fastest bitcoin mining hardware running for you....'
+    description: 'Don’t wrestle with rig assembly and hot, noisy miners at home. We have the fastest bitcoin mining hardware running for you....',
+    href: '#'
   },
   {
     id: 4,
     img: '/icons/blogs/img2.png',
     title: 'All you want to know about industrial laws.',
-    description: 'Don’t wrestle with rig assembly and hot, noisy miners at home. We have the fastest bitcoin mining hardware running for you....'
+    description: 'Don’t wrestle with rig assembly and hot, noisy miners at home. We have the fastest bitcoin mining hardware running for you....',
+    href: '#'
   },
   {
     id: 5,
     img: '/icons/blogs/img3.png',
     title: 'Lorem Ipsum dummy text of the printing.',
-    description: 'Don’t wrestle with rig assembly and hot, noisy miners at home. We have the fastest bitcoin mining hardware running for you....'
+    description: 'Don’t wrestle with rig assembly and hot, noisy miners at home. We have the fastest bitcoin mining hardware running for you....',
+    href: '#'
   },
   {
     id: 6,
     img: '/icons/blogs/img1.png',
     title: 'Lorem Ipsum is simply and typesetting.',
-    description: 'Don’t wrestle with rig assembly and hot, noisy miners at home. We have the fastest bitcoin mining hardware running for you....'
+    description: 'Don’t wrestle with rig assembly and hot, noisy miners at home. We have the fastest bitcoin mining hardware running for you....',
+    href: '#'
   },
 ]
 
