bb09eecd5bf511a9780b31b5e56ce6eab0d14117
Matheus Sá <matheus.sa.alves@outlook.com>
2021-04-14 10:58:31 -0300

    Criando pagina Contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,9 @@
+// STYLES
+import styles from '../styles/pages/Contact.module.css'
+
 // COMPONENT
 import Header from '../components/Header'
+import RouteDescription from '../components/RouteDescription'
 import Wrapper from '../components/Wrapper'
 
 // HOOKS
@@ -18,6 +22,18 @@ const Contact = () => {
   return (
     <div>
       <Header />
+      <RouteDescription>
+        <h1>Contact us</h1>
+        <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+        when an unknown printer took a galley of type and scrambled it to make a type 
+        specimen book
+        </p>
+      </RouteDescription>
+      <section>
+
+      </section>
       <Wrapper />
     </div>
   )
